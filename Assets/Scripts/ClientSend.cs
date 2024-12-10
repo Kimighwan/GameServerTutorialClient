673eb8211f67f7ef9388c2cd0abde1ec82e9579e
Kimikhwan <moonfgg@naver.com>
2024-12-10 20:09:18 +0900

    ClientSend 스크립트 주석 처리 완료

--- Assets/Scripts/ClientSend.cs
+++ Assets/Scripts/ClientSend.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 
-// 네트워크를 통해 다양한 패킷을 생성하여 전송을 정의하는 클래스
+// 패킷을 전송하는 메소드 함수들
+// 패킷을 생성하는 메소드 함수들
+
+// 위 두개의 작업을 하는 정의한 클래스
 
 public class ClientSend : MonoBehaviour
 {
-    // 아래의 Send 메서드 들은 전송할 패킷을을 준비하는 메소드
-    // 클라이언트 버퍼에 전달받은 패킷 데이터를 저장
+    // 아래의 Send 메서드들은 패킷을 전송
 
     // 중요한 정보는 TCP를 이용하고
     // 반대로 중요하지 않거나 빠르게 통신이 필요한 경우는 UDP를 사용
@@ -27,8 +29,9 @@ public class ClientSend : MonoBehaviour
         Client.instance.udp.SendData(packet);
     }
 
-    #region Packet
-    public static void WelcomeReceived() // 클라이언트가 환영 메세지를 받으면 서버로 보낼 패킷을 생성
+    #region Packet // 패킷에 필요한 데이터를 입력 // 위의 적절한 전송 메서드를 통해서 전송
+
+    public static void WelcomeReceived() // 최초 접속시 클라이언트 ID와 플레이어 이름을 담은 패킷
     {
         using (Packet packet = new Packet((int)ClientPackets.welcomeReceived))
         {
@@ -39,7 +42,7 @@ public class ClientSend : MonoBehaviour
         }
     }
 
-    public static void PlayerMovement(bool[] inputs) // 플레이어가 입력한 이동 데이터 패킷 생성후 전송
+    public static void PlayerMovement(bool[] inputs) // 플레이어가 입력한 이동 정보 패킷
     {
         using (Packet packet = new Packet((int)ClientPackets.playerMovement))
         {
@@ -55,7 +58,7 @@ public class ClientSend : MonoBehaviour
     }
 
 
-    public static void PlayerShoot(Vector3 facing)
+    public static void PlayerShoot(Vector3 facing)  // 플레이어가 총을 사용한 정보 패킷
     {
         using (Packet packet = new Packet((int)ClientPackets.playerShoot))
         {
@@ -65,7 +68,7 @@ public class ClientSend : MonoBehaviour
         }
     }
 
-    public static void PlayerThrowItem(Vector3 facing)
+    public static void PlayerThrowItem(Vector3 facing) // 플레이어가 슈류탄을 사용한 정보
     {
         using (Packet packet = new Packet((int)ClientPackets.playerThrowItem))
         {
